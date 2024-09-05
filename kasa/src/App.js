d889c50a74dd3188fc81a0e740616b99735a5f67
louanecaca <louane.augsburger@gmail.com>
2024-09-05 15:59:36 +0200

    erreur 404

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from "./Pages/Home";
 import About from "./Pages/About";
+import NotFound from "./Components/NotFound";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
      <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="*" element={<NotFound />} />
      </Routes>
     </BrowserRouter>
   );
